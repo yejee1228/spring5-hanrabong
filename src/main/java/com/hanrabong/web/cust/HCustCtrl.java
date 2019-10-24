1b4d0b9a78f2c99ac7be2a5e431b5c4b4379bd6d
yejee1228 <yejee1228@gmail.com>
2019-10-24 13:06:24 +0900

    191024 13:06

--- src/main/java/com/hanrabong/web/cust/HCustCtrl.java
+++ src/main/java/com/hanrabong/web/cust/HCustCtrl.java
@@ -4,13 +4,17 @@ package com.hanrabong.web.cust;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hanrabong.web.cmm.IConsumer;
 import com.hanrabong.web.cmm.IFunction;
+import com.hanrabong.web.cmm.ISupplier;
 import com.hanrabong.web.cust.HCust;
 import com.hanrabong.web.utl.Printer;
 
@@ -26,25 +30,15 @@ public class HCustCtrl {
 	
 	@PostMapping("/")
 	public String join(@RequestBody HCust param) {
-		printer.accept("람다 프린터가 출력한 값"+param.getCid()+", "+param.getCpw());
 		IConsumer<HCust>c = T->mapper.insertCust(param);
 		c.accept(param);
 		return "SUCCESS";
 	}
 	
-	@PostMapping("/session")
-	public HCust login (@RequestBody HCust param){
+	@PostMapping("/{cid}")
+	public HCust login (@PathVariable String cid, @RequestBody HCust param){
 		IFunction<HCust,HCust> f=T-> mapper.selectByIdPw(param);
 		return f.apply(param);
 	} 
 	
-	@PostMapping("/mypage")
-	public HCust mypage (@RequestBody HCust param){
-		logger.info("login id와 pw {}",param.getCid()+", "+param.getCpw());
-		cust.setCid(param.getCid());
-		cust.setCpw(param.getCpw());
-		logger.info("login map에 있는 사용자정보 {}",cust.toString());
-		return cust;
-	} 
-	
-}
+}//
